fbb44076f52655aa93c4349a51e50aa4737d5d1b
Wilson <wilson.shrestha@gmail.com>
2018-10-20 12:25:17 +0100

    Add iOS

---
+++ ios/fire-reporting/fire-reporting/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  fire-reporting
+//
+//  Created by Wilson on 20/10/2018.
+//  Copyright © 2018 Wilson. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
